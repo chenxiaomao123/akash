akash-network/node

feat: persistent storage
Signed-off-by: Artur Troian <troian.ap@gmail.com>

provider/cluster/kube/builder/service.go
@@ -1,0 +1,161 @@
+package builder
+
+import (
+	"fmt"
+
+	"github.com/pkg/errors"
+	"github.com/tendermint/tendermint/libs/log"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
+
+	manitypes "github.com/ovrclk/akash/manifest"
+	"github.com/ovrclk/akash/provider/cluster/util"
+	mtypes "github.com/ovrclk/akash/x/market/types"
+)
+
+type Service interface {
+	workloadBase
+	Create() (*corev1.Service, error)
+	Update(obj *corev1.Service) (*corev1.Service, error)
+	Any() bool
+}
+
+type service struct {
+	workload
+	requireNodePort bool
+}
+
+var _ Service = (*service)(nil)
+
+func NewService(log log.Logger, settings Settings, lid mtypes.LeaseID, group *manitypes.Group, mservice *manitypes.Service, requireNodePort bool) Service {
+	return &service{
+		workload:        newWorkloadBuilder(log, settings, lid, group, mservice),
+		requireNodePort: requireNodePort,
+	}
+}
+
+func (b *service) Name() string {
+	basename := b.workload.Name()
+	if b.requireNodePort {
+		return makeGlobalServiceNameFromBasename(basename)
+	}
+	return basename
+}
+
+func (b *service) workloadServiceType() corev1.ServiceType {
+	if b.requireNodePort {
+		return corev1.ServiceTypeNodePort
+	}
+	return corev1.ServiceTypeClusterIP
+}
+
+func (b *service) Create() (*corev1.Service, error) { // nolint:golint,unparam
+	ports, err := b.ports()
+	if err != nil {
+		return nil, err
+	}
+	service := &corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:   b.Name(),
+			Labels: b.labels(),
+		},
+		Spec: corev1.ServiceSpec{
+			Type:     b.workloadServiceType(),
+			Selector: b.labels(),
+			Ports:    ports,
+		},
+	}
+	b.log.Debug("provider/cluster/kube/builder: created service", "service", service)
+
+	return service, nil
+}
+
+func (b *service) Update(obj *corev1.Service) (*corev1.Service, error) { // nolint:golint,unparam
+	obj.Labels = b.labels()
+	obj.Spec.Selector = b.labels()
+	ports, err := b.ports()
+	if err != nil {
+		return nil, err
+	}
+
+	// retain provisioned NodePort values
+	if b.requireNodePort {
+
+		// for each newly-calculated port
+		for i, port := range ports {
+
+			// if there is a current (in-kube) port defined
+			// with the same specified values
+			for _, curport := range obj.Spec.Ports {
+				if curport.Name == port.Name &&
+					curport.Port == port.Port &&
+					curport.TargetPort.IntValue() == port.TargetPort.IntValue() &&
+					curport.Protocol == port.Protocol {
+
+					// re-use current port
+					ports[i] = curport
+				}
+			}
+		}
+	}
+
+	obj.Spec.Ports = ports
+	return obj, nil
+}
+
+func (b *service) Any() bool {
+	for _, expose := range b.service.Expose {
+		exposeIsIngress := util.ShouldBeIngress(expose)
+		if b.requireNodePort && exposeIsIngress {
+			continue
+		}
+
+		if !b.requireNodePort && exposeIsIngress {
+			return true
+		}
+
+		if expose.Global == b.requireNodePort {
+			return true
+		}
+	}
+	return false
+}
+
+var errUnsupportedProtocol = errors.New("Unsupported protocol for service")
+var errInvalidServiceBuilder = errors.New("service builder invalid")
+
+func (b *service) ports() ([]corev1.ServicePort, error) {
+	ports := make([]corev1.ServicePort, 0, len(b.service.Expose))
+	for i, expose := range b.service.Expose {
+		shouldBeIngress := util.ShouldBeIngress(expose)
+		if expose.Global == b.requireNodePort || (!b.requireNodePort && shouldBeIngress) {
+			if b.requireNodePort && shouldBeIngress {
+				continue
+			}
+
+			var exposeProtocol corev1.Protocol
+			switch expose.Proto {
+			case manitypes.TCP:
+				exposeProtocol = corev1.ProtocolTCP
+			case manitypes.UDP:
+				exposeProtocol = corev1.ProtocolUDP
+			default:
+				return nil, errUnsupportedProtocol
+			}
+			externalPort := util.ExposeExternalPort(b.service.Expose[i])
+			ports = append(ports, corev1.ServicePort{
+				Name:       fmt.Sprintf("%d-%d", i, int(externalPort)),
+				Port:       externalPort,
+				TargetPort: intstr.FromInt(int(expose.Port)),
+				Protocol:   exposeProtocol,
+			})
+		}
+	}
+
+	if len(ports) == 0 {
+		b.log.Debug("provider/cluster/kube/builder: created 0 ports", "requireNodePort", b.requireNodePort, "serviceExpose", b.service.Expose)
+		return nil, errInvalidServiceBuilder
+	}
+	return ports, nil
+}

provider/cluster/kube/builder/statefulset.go
@@ -1,0 +1,80 @@
+package builder
+
+import (
+	"github.com/tendermint/tendermint/libs/log"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	manitypes "github.com/ovrclk/akash/manifest"
+	mtypes "github.com/ovrclk/akash/x/market/types"
+)
+
+type StatefulSet interface {
+	workloadBase
+	Create() (*appsv1.StatefulSet, error)
+	Update(obj *appsv1.StatefulSet) (*appsv1.StatefulSet, error)
+}
+
+type statefulSet struct {
+	workload
+}
+
+var _ StatefulSet = (*statefulSet)(nil)
+
+func NewStatefulSet(log log.Logger, settings Settings, lid mtypes.LeaseID, group *manitypes.Group, service *manitypes.Service) StatefulSet {
+	return &statefulSet{
+		workload: newWorkloadBuilder(log, settings, lid, group, service),
+	}
+}
+
+func (b *statefulSet) Create() (*appsv1.StatefulSet, error) { // nolint:golint,unparam
+	replicas := int32(b.service.Count)
+	falseValue := false
+
+	var effectiveRuntimeClassName *string
+	if len(b.runtimeClassName) != 0 && b.runtimeClassName != runtimeClassNoneValue {
+		effectiveRuntimeClassName = &b.runtimeClassName
+	}
+
+	kdeployment := &appsv1.StatefulSet{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:   b.Name(),
+			Labels: b.labels(),
+		},
+		Spec: appsv1.StatefulSetSpec{
+			Selector: &metav1.LabelSelector{
+				MatchLabels: b.labels(),
+			},
+			Replicas: &replicas,
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: b.labels(),
+				},
+				Spec: corev1.PodSpec{
+					RuntimeClassName: effectiveRuntimeClassName,
+					SecurityContext: &corev1.PodSecurityContext{
+						RunAsNonRoot: &falseValue,
+					},
+					AutomountServiceAccountToken: &falseValue,
+					Containers:                   []corev1.Container{b.container()},
+				},
+			},
+			VolumeClaimTemplates: b.persistentVolumeClaims(),
+		},
+	}
+
+	return kdeployment, nil
+}
+
+func (b *statefulSet) Update(obj *appsv1.StatefulSet) (*appsv1.StatefulSet, error) { // nolint:golint,unparam
+	replicas := int32(b.service.Count)
+	obj.Labels = b.labels()
+	obj.Spec.Selector.MatchLabels = b.labels()
+	obj.Spec.Replicas = &replicas
+	obj.Spec.Template.Labels = b.labels()
+	obj.Spec.Template.Spec.Containers = []corev1.Container{b.container()}
+	obj.Spec.VolumeClaimTemplates = []corev1.PersistentVolumeClaim{}
+
+	return obj, nil
+}

chenxiaomao123/akash

provider/cluster/kube/builder/service.go
@@ -1,0 +1,161 @@
+package builder
+
+import (
+	"fmt"
+
+	"github.com/pkg/errors"
+	"github.com/tendermint/tendermint/libs/log"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
+
+	manitypes "github.com/ovrclk/akash/manifest"
+	"github.com/ovrclk/akash/provider/cluster/util"
+	mtypes "github.com/ovrclk/akash/x/market/types"
+)
+
+type Service interface {
+	workloadBase
+	Create() (*corev1.Service, error)
+	Update(obj *corev1.Service) (*corev1.Service, error)
+	Any() bool
+}
+
+type service struct {
+	workload
+	requireNodePort bool
+}
+
+var _ Service = (*service)(nil)
+
+func BuildService(log log.Logger, settings Settings, lid mtypes.LeaseID, group *manitypes.Group, mservice *manitypes.Service, requireNodePort bool) Service {
+	return &service{
+		workload:        newWorkloadBuilder(log, settings, lid, group, mservice),
+		requireNodePort: requireNodePort,
+	}
+}
+
+func (b *service) Name() string {
+	basename := b.workload.Name()
+	if b.requireNodePort {
+		return makeGlobalServiceNameFromBasename(basename)
+	}
+	return basename
+}
+
+func (b *service) workloadServiceType() corev1.ServiceType {
+	if b.requireNodePort {
+		return corev1.ServiceTypeNodePort
+	}
+	return corev1.ServiceTypeClusterIP
+}
+
+func (b *service) Create() (*corev1.Service, error) { // nolint:golint,unparam
+	ports, err := b.ports()
+	if err != nil {
+		return nil, err
+	}
+	svc := &corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:   b.Name(),
+			Labels: b.labels(),
+		},
+		Spec: corev1.ServiceSpec{
+			Type:     b.workloadServiceType(),
+			Selector: b.labels(),
+			Ports:    ports,
+		},
+	}
+	// b.log.Debug("provider/cluster/kube/builder: created service", "service", svc)
+
+	return svc, nil
+}
+
+func (b *service) Update(obj *corev1.Service) (*corev1.Service, error) { // nolint:golint,unparam
+	obj.Labels = b.labels()
+	obj.Spec.Selector = b.labels()
+	ports, err := b.ports()
+	if err != nil {
+		return nil, err
+	}
+
+	// retain provisioned NodePort values
+	if b.requireNodePort {
+
+		// for each newly-calculated port
+		for i, port := range ports {
+
+			// if there is a current (in-kube) port defined
+			// with the same specified values
+			for _, curport := range obj.Spec.Ports {
+				if curport.Name == port.Name &&
+					curport.Port == port.Port &&
+					curport.TargetPort.IntValue() == port.TargetPort.IntValue() &&
+					curport.Protocol == port.Protocol {
+
+					// re-use current port
+					ports[i] = curport
+				}
+			}
+		}
+	}
+
+	obj.Spec.Ports = ports
+	return obj, nil
+}
+
+func (b *service) Any() bool {
+	for _, expose := range b.service.Expose {
+		exposeIsIngress := util.ShouldBeIngress(expose)
+		if b.requireNodePort && exposeIsIngress {
+			continue
+		}
+
+		if !b.requireNodePort && exposeIsIngress {
+			return true
+		}
+
+		if expose.Global == b.requireNodePort {
+			return true
+		}
+	}
+	return false
+}
+
+var errUnsupportedProtocol = errors.New("Unsupported protocol for service")
+var errInvalidServiceBuilder = errors.New("service builder invalid")
+
+func (b *service) ports() ([]corev1.ServicePort, error) {
+	ports := make([]corev1.ServicePort, 0, len(b.service.Expose))
+	for i, expose := range b.service.Expose {
+		shouldBeIngress := util.ShouldBeIngress(expose)
+		if expose.Global == b.requireNodePort || (!b.requireNodePort && shouldBeIngress) {
+			if b.requireNodePort && shouldBeIngress {
+				continue
+			}
+
+			var exposeProtocol corev1.Protocol
+			switch expose.Proto {
+			case manitypes.TCP:
+				exposeProtocol = corev1.ProtocolTCP
+			case manitypes.UDP:
+				exposeProtocol = corev1.ProtocolUDP
+			default:
+				return nil, errUnsupportedProtocol
+			}
+			externalPort := util.ExposeExternalPort(b.service.Expose[i])
+			ports = append(ports, corev1.ServicePort{
+				Name:       fmt.Sprintf("%d-%d", i, int(externalPort)),
+				Port:       externalPort,
+				TargetPort: intstr.FromInt(int(expose.Port)),
+				Protocol:   exposeProtocol,
+			})
+		}
+	}
+
+	if len(ports) == 0 {
+		b.log.Debug("provider/cluster/kube/builder: created 0 ports", "requireNodePort", b.requireNodePort, "serviceExpose", b.service.Expose)
+		return nil, errInvalidServiceBuilder
+	}
+	return ports, nil
+}

provider/cluster/kube/builder/statefulset.go
@@ -1,0 +1,80 @@
+package builder
+
+import (
+	"github.com/tendermint/tendermint/libs/log"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	manitypes "github.com/ovrclk/akash/manifest"
+	mtypes "github.com/ovrclk/akash/x/market/types"
+)
+
+type StatefulSet interface {
+	workloadBase
+	Create() (*appsv1.StatefulSet, error)
+	Update(obj *appsv1.StatefulSet) (*appsv1.StatefulSet, error)
+}
+
+type statefulSet struct {
+	workload
+}
+
+var _ StatefulSet = (*statefulSet)(nil)
+
+func BuildStatefulSet(log log.Logger, settings Settings, lid mtypes.LeaseID, group *manitypes.Group, service *manitypes.Service) StatefulSet {
+	return &statefulSet{
+		workload: newWorkloadBuilder(log, settings, lid, group, service),
+	}
+}
+
+func (b *statefulSet) Create() (*appsv1.StatefulSet, error) { // nolint:golint,unparam
+	replicas := int32(b.service.Count)
+	falseValue := false
+
+	var effectiveRuntimeClassName *string
+	if len(b.runtimeClassName) != 0 && b.runtimeClassName != runtimeClassNoneValue {
+		effectiveRuntimeClassName = &b.runtimeClassName
+	}
+
+	kdeployment := &appsv1.StatefulSet{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:   b.Name(),
+			Labels: b.labels(),
+		},
+		Spec: appsv1.StatefulSetSpec{
+			Selector: &metav1.LabelSelector{
+				MatchLabels: b.labels(),
+			},
+			Replicas: &replicas,
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: b.labels(),
+				},
+				Spec: corev1.PodSpec{
+					RuntimeClassName: effectiveRuntimeClassName,
+					SecurityContext: &corev1.PodSecurityContext{
+						RunAsNonRoot: &falseValue,
+					},
+					AutomountServiceAccountToken: &falseValue,
+					Containers:                   []corev1.Container{b.container()},
+				},
+			},
+			VolumeClaimTemplates: b.persistentVolumeClaims(),
+		},
+	}
+
+	return kdeployment, nil
+}
+
+func (b *statefulSet) Update(obj *appsv1.StatefulSet) (*appsv1.StatefulSet, error) { // nolint:golint,unparam
+	replicas := int32(b.service.Count)
+	obj.Labels = b.labels()
+	obj.Spec.Selector.MatchLabels = b.labels()
+	obj.Spec.Replicas = &replicas
+	obj.Spec.Template.Labels = b.labels()
+	obj.Spec.Template.Spec.Containers = []corev1.Container{b.container()}
+	obj.Spec.VolumeClaimTemplates = []corev1.PersistentVolumeClaim{}
+
+	return obj, nil
+}